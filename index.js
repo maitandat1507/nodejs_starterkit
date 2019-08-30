098d4a03d4fe348927e60d579cf003317ca9c5cd
maitandat1507 <maitandat1507@gmail.com>
2019-08-30 12:33:36 +0700

    handle err

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const fs = require('fs'); // node's module
 
 // Non-blocking, asynchronous way
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  if (err) return console.log('ERROR!!!');
+  
   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
     console.log(data2);
     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
